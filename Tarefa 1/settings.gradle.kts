9ffb8f84f10aa2f42ce59c4a4e89003c1e06f83a
jefferson.murat <jr-murat@hotmail.com>
2023-09-22 20:28:15 -0300

    Tarefa1

---
+++ Tarefa 1/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tarefa1Ac1"
+include(":app")
+ 
