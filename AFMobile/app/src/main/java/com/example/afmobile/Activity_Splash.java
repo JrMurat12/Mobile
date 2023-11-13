0002044029c8b628fbb50d9502eecb3a9906b613
jefferson.murat <jr-murat@hotmail.com>
2023-11-13 20:24:15 -0300

    Mobile 13-11

--- AFMobile/app/src/main/java/com/example/afmobile/Activity_Splash.java
+++ AFMobile/app/src/main/java/com/example/afmobile/Activity_Splash.java
@@ -1,4 +1,4 @@
-package com.example.ac2;
+package com.example.afmobile;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class SplashActivity extends AppCompatActivity {
+public class Activity_Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +19,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent = new Intent(Activity_Splash.this, Activity_Login.class);
                 startActivity(intent);
 
                 finish();
